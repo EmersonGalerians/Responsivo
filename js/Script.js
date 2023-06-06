f852804a23524349737038f4bf1226d77644ce42
EmersonGalerians <106710578+EmersonGalerians@users.noreply.github.com>
2023-06-06 17:32:35 -0300

    primeiroLogin

--- js/Script.js
+++ js/Script.js
@@ -1,21 +1,25 @@
-// Variáveis para armazenar o email e a senha
-let nome = '';
-let email = '';
-let senha = '';
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
 
-// Função para salvar os dados
-function salvarDados() {
-  nome = document.getElementById('nameInput').value;
-  email = document.getElementById('emailInput').value;
-  senha = document.getElementById('senhaInput').value;
-  console.log('Dados salvos:' ,nome, email, senha);
-}
+loginLink.addEventListener('click', () => {
+  wrapper.classList.add('active');
 
-// Função para executar quando o formulário for enviado
-function handleSubmit(event) {
-  event.preventDefault();
-  salvarDados();
-}
+})
 
-// Adiciona um ouvinte de evento para o formulário
-document.getElementById('meuForm').addEventListener('submit', handleSubmit);
+registerLink.addEventListener('click', () => {
+  wrapper.classList.remove('active');
+
+})
+
+btnPopup.addEventListener('click', () => {
+  wrapper.classList.add('active-popup');
+
+})
+
+iconClose.addEventListener('click', () => {
+  wrapper.classList.remove('active-popup');
+
+})
